aedab8e74fbbdbd45fdae3df35a6e2aaaaecb4f5
shnaki <tehnca-nome@hotmail.co.jp>
2020-05-12 10:23:44 +0900

    Add go-adodb sample

--- main.go
+++ main.go
@@ -1,48 +1,59 @@
 package main
 
 import (
-	"os"
+	"database/sql"
+	"fmt"
+	"strings"
 
-	"github.com/olekukonko/tablewriter"
+	"github.com/go-ole/go-ole"
+	_ "github.com/mattn/go-adodb"
 )
 
 func main() {
-	data := [][]string{
-		{"p01", "free", "0.15", "511", "1", "0", "0", "6/3", "0", ""},
-		{"p02", "free", "0.00", "511", "1", "0", "0", "0/0", "0", ""},
-		{"p03", "free", "0.00", "511", "1", "0", "0", "0/0", "0", ""},
-		{"p04", "free", "0.00", "511", "1", "0", "0", "0/0", "0", ""},
-		{"p05", "free", "0.00", "511", "1", "0", "0", "0/0", "0", ""},
-		{"p06", "free", "0.00", "511", "1", "0", "0", "0/0", "0", ""},
-		{"p07", "excl", "0.73", "511", "1", "0", "0", "1/1", "0", "219"},
-		{"p08", "excl", "0.94", "511", "1", "0", "0", "1/1", "0", "217"},
-		{"p09", "excl", "0.99", "511", "1", "0", "0", "1/1", "0", "217"},
+	ole.CoInitialize(0)
+	defer ole.CoUninitialize()
+
+	f := "./testdata/mme_code.mdb"
+	//provider := "Microsoft.ACE.OLEDB.12.0"
+	provider := "Microsoft.Jet.OLEDB.4.0"
+
+	db, err := sql.Open("adodb", "Provider="+provider+";Data Source="+f+";")
+	if err != nil {
+		fmt.Println("open", err)
+		return
 	}
+	defer db.Close()
 
-	// Original output is below.
-	//node state  load    pmem ncpu   mem   resi usrs jobs   jobids
-	// p01  free  0.15     511   1      0      0  6/3    0
-	// p02  free  0.00     511   1      0      0  0/0    0
-	// p03  free  0.00     511   1      0      0  0/0    0
-	// p04  free  0.00     511   1      0      0  0/0    0
-	// p05  free  0.00     511   1      0      0  0/0    0
-	// p06  free  0.00     511   1      0      0  0/0    0
-	// p07  excl  0.73     511   1      0      0  1/1    1   219
-	// p08  excl  0.94     511   1      0      0  1/1    1   217
-	// p09  excl  0.99     511   1      0      0  1/1    1   217
-
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeaderLine(false)
-	table.SetHeader([]string{"node", "state", "load", "pmem", "ncpu", "mem", "resi", "usrs", "jobs", "jobids"})
-	table.SetAutoWrapText(false)
-	table.SetAutoFormatHeaders(false)
-	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-	table.SetBorder(false)
-	//table.SetCenterSeparator("")
-	//table.SetColumnSeparator("")
-	//table.SetRowSeparator("")
-	table.SetNoWhiteSpace(true)
-	table.SetTablePadding(" ")
-	table.AppendBulk(data)
-	table.Render()
+	// Main Location
+	query := "select" +
+		" mme_transducer_main_locations.[type]," +
+		" mme_transducer_main_locations.[trans_main_loc]," +
+		" mme_transducer_main_locations.[text_l1]," +
+		" sys_data_history.dataversion," +
+		" mme_transducer_main_locations.[reamrks]" +
+		" from sys_data_history inner join mme_transducer_main_locations" +
+		" on sys_data_history.[id] = mme_transducer_main_locations.[version]" +
+		" order by mme_transducer_main_locations.[type]," +
+		" mme_transducer_main_locations.sortkey;"
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Println("select", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var locationType string
+		var code string
+		var name string
+		var version string
+		var remarks *string
+		err = rows.Scan(&locationType, &code, &name, &version, &remarks)
+		if err != nil {
+			fmt.Println("scan", err)
+			return
+		}
+		s := strings.ReplaceAll(name, " ", "_")
+		s = strings.ReplaceAll(name, ",", "_")
+		fmt.Printf("%s,  // %s\n", strings.ToUpper(s), version)
+	}
 }
