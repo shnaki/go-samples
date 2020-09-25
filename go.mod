6db1b87a38ac8c726ff7705baf499c18fde8a953
shnaki <tehnca-nome@hotmail.co.jp>
2020-09-25 13:19:54 +0900

    feat: Add example of fyne

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module go-samples
 
 go 1.13
 
-require (
-	github.com/go-ole/go-ole v1.2.4
-	github.com/mattn/go-adodb v0.0.1
-	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
-)
+require fyne.io/fyne v1.3.3
--- main.go
+++ main.go
@@ -1,59 +1,20 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"strings"
-
-	"github.com/go-ole/go-ole"
-	_ "github.com/mattn/go-adodb"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 )
 
 func main() {
-	ole.CoInitialize(0)
-	defer ole.CoUninitialize()
+	a := app.New()
 
-	f := "./testdata/mme_code.mdb"
-	//provider := "Microsoft.ACE.OLEDB.12.0"
-	provider := "Microsoft.Jet.OLEDB.4.0"
+	w := a.NewWindow("Hello")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello Fyne!"),
+		widget.NewButton("Quit", func() {
+			a.Quit()
+		}),
+	))
 
-	db, err := sql.Open("adodb", "Provider="+provider+";Data Source="+f+";")
-	if err != nil {
-		fmt.Println("open", err)
-		return
-	}
-	defer db.Close()
-
-	// Main Location
-	query := "select" +
-		" mme_transducer_main_locations.[type]," +
-		" mme_transducer_main_locations.[trans_main_loc]," +
-		" mme_transducer_main_locations.[text_l1]," +
-		" sys_data_history.dataversion," +
-		" mme_transducer_main_locations.[reamrks]" +
-		" from sys_data_history inner join mme_transducer_main_locations" +
-		" on sys_data_history.[id] = mme_transducer_main_locations.[version]" +
-		" order by mme_transducer_main_locations.[type]," +
-		" mme_transducer_main_locations.sortkey;"
-	rows, err := db.Query(query)
-	if err != nil {
-		fmt.Println("select", err)
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var locationType string
-		var code string
-		var name string
-		var version string
-		var remarks *string
-		err = rows.Scan(&locationType, &code, &name, &version, &remarks)
-		if err != nil {
-			fmt.Println("scan", err)
-			return
-		}
-		s := strings.ReplaceAll(name, " ", "_")
-		s = strings.ReplaceAll(name, ",", "_")
-		fmt.Printf("%s,  // %s\n", strings.ToUpper(s), version)
-	}
+	w.ShowAndRun()
 }
